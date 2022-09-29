f6b608a9d285d585eaafe33c0b7fce629d5fd93b
Abdul Hafiz Bahrain <abdulhafizbahrain@gmail.com>
2022-09-29 17:36:11 +0800

    p

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // import './main.js'
 // import 'https://cdn.jsdelivr.net/gh/abdulhafizbahrain/affscript/main.js'
-import 'https://cdn.jsdelivr.net/gh/abdulhafizbahrain/affscript@master/main.js'
+// import 'https://cdn.jsdelivr.net/gh/abdulhafizbahrain/affscript@master/main.js'
+
+import 'https://raw.githubusercontent.com/abdulhafizbahrain/affscript/main/main.js'
 
 // alertRedirect();
 // linkRedirect();
